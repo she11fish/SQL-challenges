0ade82d073a906cfabdc8a7e54af3dbcd6559d8a
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-04-27 10:50:10 -0400

    Confirmation Rate

---
+++ Leetcode/Medium/Confirmation Rate/solution.sql
@@ -0,0 +1,11 @@
+SELECT 
+    Signups.user_id,
+    ROUND(AVG(CASE WHEN Confirmations.action = 'confirmed' THEN 1 ELSE 0 END), 2) AS confirmation_rate
+FROM 
+    Signups
+LEFT JOIN 
+    Confirmations
+ON 
+    Signups.user_id = Confirmations.user_id
+GROUP BY 
+    user_id;
