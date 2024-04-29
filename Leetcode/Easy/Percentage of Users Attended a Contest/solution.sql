159569ea0a778989f9f599be584ff5d2e65fa27e
Waheeb Barqawi <trolltrolltrolllollollol@outlook.com>
2024-04-29 11:17:52 -0400

    solved Percentage of Users Attended a Contest

---
+++ Leetcode/Easy/Percentage of Users Attended a Contest/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    contest_id,
+    ROUND(COUNT(contest_id) / (SELECT COUNT(user_id) FROM Users) * 100, 2) as percentage
+FROM
+    Register
+GROUP BY
+    contest_id
+ORDER BY percentage DESC, contest_id ASC;
